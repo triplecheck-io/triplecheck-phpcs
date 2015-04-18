fcbcd2b6f9186da0c0a6ef4572de2b1ec30f03d9
lfolco <me@laurafolco.com>
2015-04-18 16:05:13 -0700

    added test for configure

--- src/Triplecheck/Phpcs/Plugin.php
+++ src/Triplecheck/Phpcs/Plugin.php
@@ -2,10 +2,17 @@
 
 namespace Triplecheck\Phpcs;
 
-class Plugin extends \Triplecheck\Common\AbstractPlugin implements \Triplecheck\Common\Plugin\RunnableInterface
+class Plugin extends \Triplecheck\Common\AbstractPlugin implements
+    \Triplecheck\Common\Plugin\ConfigurableInterface,
+    \Triplecheck\Common\Plugin\RunnableInterface
 {
 
     public function run()
+    {
+        return;
+    }
+
+    public function configure()
     {
         return;
     }
